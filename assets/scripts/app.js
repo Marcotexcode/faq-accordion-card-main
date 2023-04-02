cb18b3146508072a5dcb64a3f035b30510110542
marco tex <marcotexcode@gmail.com>
2023-04-03 00:29:56 +0200

    Responsive css correction

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,13 @@
 
+const paraghAll = document.querySelector('.card_body_right_paragh');
+
 containerText.forEach(el => {
     const arrow = el.querySelector('.arrow');
     const paragh = el.querySelector('.card_body_right_paragh');
 
     arrow.addEventListener("click", function(){
+        paraghAll.classList.remove('active');
+
         arrow.style.transform = arrow.style.transform === 'rotate(180deg)' ? '' : 'rotate(180deg)';
         paragh.classList.toggle('active');
     });

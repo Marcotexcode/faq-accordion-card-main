5b91c29ea8c900e0a634b00a291ab49326135288
Marco <marcotexcode@gmail.com>
2023-04-04 16:34:19 +0200

    Correction js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,27 @@
 
-const paraghAll = document.querySelector('.card_body_right_paragh');
+const paraghAll = document.querySelectorAll('.card_body_right_paragh');
+const arrowAll = document.querySelectorAll('.arrow');
 
 containerText.forEach(el => {
     const arrow = el.querySelector('.arrow');
     const paragh = el.querySelector('.card_body_right_paragh');
 
     arrow.addEventListener("click", function(){
-        paraghAll.classList.remove('active');
+
+        paraghAll.forEach(element => {
+           if (element !== paragh) {
+               element.classList.remove('active');
+           }
+        });
+
+        arrowAll.forEach(el => {
+            if (el !== arrow) {
+                el.style.transform = '';
+            }
+        });
 
         arrow.style.transform = arrow.style.transform === 'rotate(180deg)' ? '' : 'rotate(180deg)';
+
         paragh.classList.toggle('active');
     });
 });
